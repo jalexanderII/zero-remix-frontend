ed9c04ab12532e79235667ee76706e3d1f27ee43
Joel Alexander <joelalexander@lyft.com>
2023-02-02 18:12:35 -0500

    fix root and client entry

--- app/root.tsx
+++ app/root.tsx
@@ -1,21 +1,25 @@
-import type { LoaderFunction, MetaFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunction, MetaFunction } from "@remix-run/node";
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import { rootAuthLoader } from "@clerk/remix/ssr.server";
 import { ClerkApp, ClerkCatchBoundary } from "@clerk/remix";
 import clark_styles from "~/styles/shared.css";
 import styles from "./styles/app.css";
 import Header from "~/components/Header";
+import StylesContext from "~/styles/stylesContext";
+import React, { useContext } from "react";
 
-export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
-};
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
 
-export function links() {
+export const links: LinksFunction = () => {
   return [
     { rel: "stylesheet", href: clark_styles },
     { rel: "stylesheet", href: styles },
   ];
-}
+};
 
 export const loader: LoaderFunction = (args) => {
   return rootAuthLoader(
@@ -29,23 +33,53 @@ export const loader: LoaderFunction = (args) => {
   );
 };
 
-function App() {
+function Document({ children, title }: { children: React.ReactNode; title?: string }) {
+  const styleData = useContext(StylesContext);
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        {title ? <title>{title}</title> : null}
         <Meta />
         <Links />
+        {styleData?.map(({ key, ids, css }) => (
+          <style
+            key={key}
+            data-emotion={`${key} ${ids.join(" ")}`}
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{ __html: css }}
+          />
+        ))}
       </head>
       <body>
-        <Header />
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+function App() {
+  return (
+    <Document>
+      <Header />
+      <Outlet />
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <Document title="Error!">
+      <Header />
+      <div>
+        <h1>Error</h1>
+        <p>{error.message}</p>
+        <p>The stack trace is:</p>
+        <pre>{error.stack}</pre>
+      </div>
+    </Document>
   );
 }
 
