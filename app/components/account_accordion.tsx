3d82d04b9fe79737d788d5e8cc2c1faba8d12e59
Joel Alexander <joelalexander@lyft.com>
2023-03-14 15:27:21 -0400

    try different config

--- app/components/account_accordion.tsx
+++ app/components/account_accordion.tsx
@@ -6,14 +6,8 @@ import {
   Block,
 } from "@tremor/react";
 import React from "react";
-import type {
-  AccountAndTransactions,
-  SlimAccount,
-  SlimTransaction,
-} from "~/utils/types.server";
+import type { AccountAndTransactions, SlimAccount } from "~/utils/types.server";
 import { PaymentPlanTransactions } from "~/components/TrxnTableWithCheckbox/transactions_table_with_checkbox";
-import { toUSD } from "~/utils/helpers";
-import { ObjectId } from "bson";
 
 interface props {
   accountAndTransactions: AccountAndTransactions;
@@ -48,53 +42,53 @@ export const AccountAccordion: React.FC<props> = ({
   );
 };
 
-const AccountTrxnItem = (accountAndTransactions: AccountAndTransactions) => {
-  const transaction: Array<SlimTransaction[]> = [];
-  for (const acc of accountAndTransactions.slimAccounts) {
-    const a: SlimTransaction[] =
-      // @ts-ignore
-      accountAndTransactions.transactionDict[acc.accountId];
-    if (a) {
-      transaction.push(a);
-    }
-  }
-
-  return (
-    <AccordionList>
-      {accountAndTransactions.slimAccounts.map(
-        (i: SlimAccount, idx: number) => (
-          <Accordion key={i.accountId}>
-            <AccordionHeader>{i.name}</AccordionHeader>
-            <AccordionBody>
-              <PaymentPlanTransactions
-                idx={idx}
-                transactions={
-                  transaction.length == 0
-                    ? makeTrxnForFullAccount(i)
-                    : transaction[idx]
-                }
-                accountId={i.accountId}
-              />
-            </AccordionBody>
-          </Accordion>
-        )
-      )}
-    </AccordionList>
-  );
-};
-
-const makeTrxnForFullAccount = (acc: SlimAccount) => {
-  const transactions: SlimTransaction[] = [];
-  const transactionId = new ObjectId().toString();
-  transactions.push({
-    id: transactionId,
-    accountId: acc.accountId,
-    userId: acc.userId,
-    name: "Account current balance",
-    amount: toUSD(acc.balance),
-    value: acc.balance,
-    date: "n/a",
-    transactionId: transactionId,
-  });
-  return transactions;
-};
+// const AccountTrxnItem = (accountAndTransactions: AccountAndTransactions) => {
+//   const transaction: Array<SlimTransaction[]> = [];
+//   for (const acc of accountAndTransactions.slimAccounts) {
+//     const a: SlimTransaction[] =
+//       // @ts-ignore
+//       accountAndTransactions.transactionDict[acc.accountId];
+//     if (a) {
+//       transaction.push(a);
+//     }
+//   }
+//
+//   return (
+//     <AccordionList>
+//       {accountAndTransactions.slimAccounts.map(
+//         (i: SlimAccount, idx: number) => (
+//           <Accordion key={i.accountId}>
+//             <AccordionHeader>{i.name}</AccordionHeader>
+//             <AccordionBody>
+//               <PaymentPlanTransactions
+//                 idx={idx}
+//                 transactions={
+//                   transaction.length == 0
+//                     ? makeTrxnForFullAccount(i)
+//                     : transaction[idx]
+//                 }
+//                 accountId={i.accountId}
+//               />
+//             </AccordionBody>
+//           </Accordion>
+//         )
+//       )}
+//     </AccordionList>
+//   );
+// };
+//
+// const makeTrxnForFullAccount = (acc: SlimAccount) => {
+//   const transactions: SlimTransaction[] = [];
+//   const transactionId = new ObjectId().toString();
+//   transactions.push({
+//     id: transactionId,
+//     accountId: acc.accountId,
+//     userId: acc.userId,
+//     name: "Account current balance",
+//     amount: toUSD(acc.balance),
+//     value: acc.balance,
+//     date: "n/a",
+//     transactionId: transactionId,
+//   });
+//   return transactions;
+// };
--- remix.config.js
+++ remix.config.js
@@ -1,20 +1,49 @@
+// module.exports = {
+//   // serverBuildTarget: "vercel",
+//   // When running locally in development mode, we use the built-in remix
+//   // server. This does not understand the vercel lambda module format,
+//   // so we default back to the standard build output.
+//   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+//   cacheDirectory: "./node_modules/.cache/remix",
+//   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+//   serverDependenciesToBundle: ["axios"],
+//   future: {
+//     v2_routeConvention: true,
+//   },
+//   appDirectory: "app",
+//   assetsBuildDirectory: "public/build",
+//   serverBuildPath: "build/index.js",
+//   publicPath: "/build/",
+//   devServerPort: 8002,
+// };
+
+const path = require("node:path");
+
+const glob = require("glob");
+
+const packages = glob
+  .sync("packages/**/package.json", {
+    cwd: path.join(__dirname, "..", ".."),
+    ignore: ["**/node_modules/**"],
+    absolute: true,
+  })
+  .map((pkg) => path.dirname(pkg));
+
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
   serverBuildTarget: "vercel",
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-  // devServerPort: 8002,
-  // When running locally in development mode, we use the built-in remix
+  appDirectory: "app",
+  ignoredRouteFiles: [".*", "**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  assetsBuildDirectory: "public/build",
+  // When running locally in development mode, we use the built in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   cacheDirectory: "./node_modules/.cache/remix",
-  ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
-  serverDependenciesToBundle: ["axios"],
+  serverDependenciesToBundle: [/.*/],
+  watchPaths: packages,
   future: {
     v2_routeConvention: true,
   },
