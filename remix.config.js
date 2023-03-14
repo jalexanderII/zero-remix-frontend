54f2426c407c96fb1cf3e44be8ba0cd8cc3ae42d
Joel Alexander <joelalexander@lyft.com>
2023-03-14 12:56:51 -0400

    try old remix config

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,7 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  // serverBuildTarget: "vercel",
+  serverBuildTarget: "vercel",
   // When running locally in development mode, we use the built-in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
@@ -13,9 +13,9 @@ module.exports = {
   // future: {
   //   v2_routeConvention: true,
   // },
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  serverBuildPath: "api/index.js",
-  publicPath: "/build/",
-  devServerPort: 8002,
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "api/index.js",
+  // publicPath: "/build/",
+  // devServerPort: 8002,
 };
