1e1c0a3d56bcb879da641d99c253b7eaebcc8d0f
Joel Alexander <joelalexander@lyft.com>
2023-03-14 15:59:06 -0400

    log remove

--- app/utils/helpers.ts
+++ app/utils/helpers.ts
@@ -7,9 +7,7 @@ export const toUSD = (value: number, digits = 2) => {
     //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
     maximumFractionDigits: digits, // (causes 2500.99 to be printed as $2,501)
   });
-  const out = formatter.format(value);
-  console.log("toUSD: ", out);
-  return out;
+  return formatter.format(value);
 };
 
 const month = [
